742c0718ab3f1f81c33eba1c42bbb36bcac75ead
csh928 <csh928@gmail.com>
2020-10-19 23:19:09 +0900

    add Routes

---
+++ src/component/Movie.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import "./Movie.css";
+
+function Movie({ id, year, title, summary, poster, genres }) {
+  return (
+    <Link
+      to={{
+        pathname: "/movie-detail",
+        state: {
+          year,
+          title,
+          summary,
+          poster,
+          genres,
+        },
+      }}
+    >
+      <div className="movie">
+        <img src={poster} alt={title} title={title} />
+        <div>
+          <h3 className="movie_title">{title}</h3>
+          <h5 className="movie_year">{year}</h5>
+        </div>
+        <div>
+          <p className="movie_summary">{summary}</p>
+        </div>
+        <div>
+          <ul className="genres">
+            {genres.map((genre, index) => (
+              <li key={index} className="genres_genre">
+                {genre}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </Link>
+  );
+}
+
+Movie.propTypes = {
+  id: PropTypes.number.isRequired,
+  year: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
+  summary: PropTypes.string.isRequired,
+  poster: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string.isRequired),
+};
+
+export default Movie;
---
+++ src/routes/About.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function About() {
+  return <span>About this page: I built it because</span>;
+}
+
+export default About;
